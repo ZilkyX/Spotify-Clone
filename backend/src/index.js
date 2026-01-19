6c30aa119e49f7d217fb1eeeb72c334a2383714d
Zilyan Kyle Lorenzo <zilyankyle08@gmail.com>
2026-01-19 22:53:32 +0800

    Routes setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const app = express();
 
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/album", albumRoutes);
+app.use("/api/stats", albumRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on PORT:", PORT);
