8c33eb74cc1134569870b21c6c5ac4bfc49a72f1
Zilyan Kyle Lorenzo <zilyankyle08@gmail.com>
2026-01-21 21:21:29 +0800

    MongoDB setup and User model creation

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,17 +6,21 @@ import adminRoutes from "./routes/admin.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-app.use("/api/users", userRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/songs", songRoutes);
+app.use("/api/song", songRoutes);
 app.use("/api/album", albumRoutes);
 app.use("/api/stats", albumRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server running on PORT:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on PORT:", PORT);
+  });
 });
