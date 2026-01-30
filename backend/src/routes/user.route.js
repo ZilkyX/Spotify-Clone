46bc90467eb006f6e6608ea9ae575c6acf1d49b9
Zilyan Kyle Lorenzo <zilyankyle08@gmail.com>
2026-01-30 18:25:01 +0800

    Added getAllUsers

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { protectRoutes } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User route");
-});
+router.get("/", protectRoutes, getAllUsers);
 
 export default router;
